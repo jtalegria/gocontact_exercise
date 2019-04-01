5f7e86e31585b067028caf71cb681523b145b27e
João Alegria <joaotalegria@ua.pt>
2019-04-01 01:45:56 +0100

    Frontend | Form Validation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import WeatherAPI from "./components/WeatherAPI"
 
 import StyledForm from "../src/styles/Form"
+import CenteredDiv from './styles/CenteredDiv';
+import SunIcon from "./gifs/sun.png"
 
 class App extends Component {
   constructor() {
@@ -11,7 +13,8 @@ class App extends Component {
       city1: "",
       city2: "",
       city3: "",
-      submitted: false
+      submitted: false,
+      submittedCount: 0
     }
     this.handleChange = this.handleChange.bind(this)
   }
@@ -19,69 +22,86 @@ class App extends Component {
   handleChange(event) {
     const { name, value } = event.target
     this.setState({
-      [name]: value
+      [name]: value,
+      submitted: false
     })
   }
 
   handleSubmit(event) {
     event.preventDefault()
-    if (this.state.submitted === true) {
-      this.setState({ submitted: !this.state.submitted })
-      // this.setState({ submitted: !this.state.submitted })
-    }
-    else {
-      this.setState({
-        submitted: true
+      this.setState({ 
+        submitted: !this.state.submitted,
+        submittedCount: this.state.submittedCount + 1
       })
-    }
   }
 
   render() {
     return (
-      <main>
-        <StyledForm>
-          <ul>
-            <li>
-              <label for="city1">City 1</label>
-              <input type="text"
-                name="city1"
-                maxlength="100"
-                value={this.setState.city1}
-                onChange={this.handleChange}>
-              </input>
-            </li>
+      <div>
+        <CenteredDiv>
+          <div id="left">
+            <CenteredDiv><h1>Weather API UI</h1></CenteredDiv>
+          </div>
+          <div id="middle">
+            <StyledForm>
+              <ul>
+                <li>
+                  <label for="city1">City 1</label>
+                  <input type="text"
+                    name="city1"
+                    value={this.setState.city1}
+                    onChange={this.handleChange}>
+                  </input>
+                </li>
 
-            <li>
-              <label for="city2">City 2</label>
-              <input type="text"
-                name="city2"
-                maxlength="100"
-                value={this.setState.city1}
-                onChange={this.handleChange}>
-              </input>
-            </li>
+                <li>
+                  <label for="city2">City 2</label>
+                  <input type="text"
+                    name="city2"
+                    value={this.setState.city1}
+                    onChange={this.handleChange}>
+                  </input>
+                </li>
 
-            <li>
-              <label for="city3">City 3</label>
-              <input type="text"
-                name="city3"
-                maxlength="100"
-                value={this.setState.city1}
-                onChange={this.handleChange}>
-              </input>
-            </li>
+                <li>
+                  <label for="city3">City 3</label>
+                  <input type="text"
+                    name="city3"
+                    value={this.setState.city1}
+                    onChange={this.handleChange}>
+                  </input>
+                </li>
 
-            <li>
-              <input type="button" onClick={(e) => this.handleSubmit(e)} value="Send" />
-            </li>
-          </ul>
-        </StyledForm>
+                <li>
+                  <input type="button" onClick={(e) => this.handleSubmit(e)} value="Send" />
+                </li>
+              </ul>
+            </StyledForm>
 
-        {this.state.submitted
-          ? <WeatherAPI city1={this.state.city1} city2={this.state.city2} city3={this.state.city3} />
+          </div>
+          <div id="right">
+            <CenteredDiv>
+              <img id="sun" src={SunIcon} alt="sun" width="250" height="250" />
+            </CenteredDiv>
+          </div>
+        </CenteredDiv>
+
+        {this.state.submittedCount !== 0 && this.state.city1 === ""
+          ? <CenteredDiv>You must provide the name of the City 1. </CenteredDiv>
           : null}
 
-      </main >
+        {this.state.submittedCount !== 0 && this.state.city2 === ""
+          ? <CenteredDiv>You must provide the name of the City 2. </CenteredDiv>
+          : null}
+
+        {this.state.submittedCount !== 0 && this.state.city3 === ""
+          ? <CenteredDiv>You must provide the name of the City 3. </CenteredDiv>
+          : null}
+
+        {this.state.submitted && this.state.city1 !== "" && this.state.city2 !== "" && this.state.city3 !== ""
+          ? <WeatherAPI city1={this.state.city1} city2={this.state.city2} city3={this.state.city3} />
+          : null}
+      </div>
     )
   }
 }
--- frontend/src/styles/CenteredDiv.js
+++ frontend/src/styles/CenteredDiv.js
@@ -5,10 +5,74 @@ const CenteredDiv = styled.div`
     height: 100%;
     align-items: center;
     justify-content: center;
-
-    @media(max-width: 1070px){
-        flex-direction: column;
+    
+    .h1 {
+        font-size: 2.5rem;
     }
+
+    #left {
+        flex-basis:50%;
+        order: 0
+    }
+    
+    #middle {
+        flex-basis:50%;
+        order: 1
+    }
+
+    #right {
+        flex-basis:50%;
+        order: 2 
+    }
+
+    @media(max-width: 599px){       /* Phones */
+        flex-direction: column;
+        
+        h1 {
+            font-size: 1.75rem;
+        }
+
+        #left{
+
+        }
+
+        
+        #right{
+            order:-1
+        }
+
+        #sun{
+            width: 100px;
+            height: 100px;
+        }
+    }
+
+    @media(min-width: 600px), @media (min-width:768px){       /* iPad */
+        flex-direction: row;
+
+        #chart {
+            flex-basis: 40%;
+            margin: 0px 25px 0px 25px;
+        }
+
+        #table {
+            flex-basis: 60%;
+            margin: 0px 25px 0px 0px;
+        }
+        
+        h1 {
+            font-size: 2.1rem;
+        }
+    }
+
+    @media (min-width: 992px){
+        flex-direction: row;
+        h1 {
+            font-size: 4rem;
+        }
+    }
+
+    
 `
 
 export default CenteredDiv
